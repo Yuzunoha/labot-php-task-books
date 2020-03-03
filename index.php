dc0ee2611a608021c143fedeb2c57c2692847078
Yuzunoha <studysteadystandby@gmail.com>
2020-03-03 16:32:17 +0900

    テーブルにできた

--- index.php
+++ index.php
@@ -1,8 +1,42 @@
 <?php
 
-$php = file_get_contents('/var/www/labot/labot-php-task-books-json/php.json');
-$js = file_get_contents('/var/www/labot/labot-php-task-books-json/javascript.json');
+$php = json_decode(file_get_contents('/var/www/labot/labot-php-task-books-json/php.json'));
+$js = json_decode(file_get_contents('/var/www/labot/labot-php-task-books-json/javascript.json'));
 
-echo "<pre>";
-print_r($php);
-print_r($js);
+?>
+
+<h1>PHPの参考書</h1>
+<table>
+<tr>
+<th>title</th>
+<th>author</th>
+<th>publisher</th>
+</tr>
+<?php
+foreach ($php as $book) {
+    echo '<tr>';
+    echo '<td>' . $book->title . '</td>';
+    echo '<td>' . $book->author . '</td>';
+    echo '<td>' . $book->publisher . '</td>';
+    echo '</tr>';
+}
+?>
+</table>
+
+<h1>JavaScriptの参考書</h1>
+<table>
+<tr>
+<th>title</th>
+<th>author</th>
+<th>publisher</th>
+</tr>
+<?php
+foreach ($js as $book) {
+    echo '<tr>';
+    echo '<td>' . $book->title . '</td>';
+    echo '<td>' . $book->author . '</td>';
+    echo '<td>' . $book->publisher . '</td>';
+    echo '</tr>';
+}
+?>
+</table>
